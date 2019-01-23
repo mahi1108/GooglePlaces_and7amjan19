2e4e51060f20eb2e3bcd928f84691f926cf2c47d
maheshthippala <mahesh.cubex@gmail.com>
2019-01-23 09:05:09 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/googleplaces_and7amjan19/MainActivity.kt
@@ -0,0 +1,12 @@
+package cubex.mahesh.googleplaces_and7amjan19
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  // AIzaSyDdCGdR2cnWw0AB0LeN3jOTjKmkKag4tew
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
